506c6766d397d9307718e653ae9a15f74708bf78
carlosredolar <carlos.redolar.torres@gmail.com>
2019-12-19 17:33:43 +0100

    people

--- Dodging_Driver/ModuleSceneIntro.cpp
+++ Dodging_Driver/ModuleSceneIntro.cpp
@@ -41,24 +41,47 @@ bool ModuleSceneIntro::Start()
 	CreateBox(-10, { 1, 0, 0 }, { 0, 1, 100 }, { 40,1,20 }, Gray);
 	CreateBox(-30, { 1, 0, 0 }, { 0, 1, 950 }, { 40,1,20 }, Gray);
 	//trees
-	for (float i = 0; i < 8; i++)
+	for (float i = 0; i < 7; i++)
 	{
 		float distance = i * 100;
 		CreateBox(0, { 0, 1, 0 }, { 10, 3, distance }, { 1,10,1 }, Brown);
 
-		CreateBox(0, { 0, 1, 0 }, { -5, 3, distance +20 }, { 1,10,1 }, Brown);
+		CreateBox(0, { 0, 1, 0 }, { -5, 3, distance +40 }, { 1,10,1 }, Brown);
 
-		CreateBox(0, { 0, 1, 0 }, { -5, 3, distance +70 }, { 1,10,1 }, Brown);
+		CreateBox(0, { 0, 1, 0 }, { -5, 3, distance +90 }, { 1,10,1 }, Brown);
 
 		CreateBox(0, { 0, 1, 0 }, { 5, 3, distance +120 }, { 1,10,1 }, Brown);
 
 		CreateBox(0, { 0, 1, 0 }, { -15, 3, distance +110 }, { 1,10,1 }, Brown);
 
-		CreateBox(0, { 0, 1, 0 }, { 15, 3, distance + 140 }, { 1,10,1 }, Brown);
+		CreateBox(0, { 0, 1, 0 }, { 15, 3, distance + 160 }, { 1,10,1 }, Brown);
 
-		CreateBox(0, { 0, 1, 0 }, { 0, 3, distance + 160 }, { 1,10,1 }, Brown);
+		CreateBox(0, { 0, 1, 0 }, { 0, 3, distance + 205 }, { 1,10,1 }, Brown);
 	}
+	//people
+	CreateBox(20, { 0, 0, 1 }, { 10, 3, 50 }, { 1,4,1 }, Black);
+	CreateBox(-20, { 0, 0, 1 }, { 8, 3, 50 }, { 1,4,1 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { 9, 7, 50 }, { 1,5,1 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { 9, 9.5, 50 }, { 2,2,2 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { 9, 7.5, 50 }, { 4,1,1 }, Black);
 
+	CreateBox(20, { 0, 0, 1 }, { -10, 3, 200 }, { 1,4,1 }, Black);
+	CreateBox(-20, { 0, 0, 1 }, { -12, 3, 200 }, { 1,4,1 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { -11, 7, 200 }, { 1,5,1 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { -11, 9.5, 200 }, { 2,2,2 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { -11, 7.5, 200 }, { 4,1,1 }, Black);
+
+	CreateBox(20, { 0, 0, 1 }, { 5, 3, 450 }, { 1,4,1 }, Black);
+	CreateBox(-20, { 0, 0, 1 }, { 3, 3, 450 }, { 1,4,1 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { 4, 7, 450 }, { 1,5,1 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { 4, 9.5, 450 }, { 2,2,2 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { 4, 7.5, 450 }, { 4,1,1 }, Black);
+
+	CreateBox(20, { 0, 0, 1 }, { -7, 3, 700 }, { 1,4,1 }, Black);
+	CreateBox(-20, { 0, 0, 1 }, { -9, 3, 700 }, { 1,4,1 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { -8, 7, 700 }, { 1,5,1 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { -8, 9.5, 700 }, { 2,2,2 }, Black);
+	CreateBox(0, { 0, 0, 1 }, { -8, 7.5, 700 }, { 4,1,1 }, Black);
 
 
 	return ret;
@@ -99,17 +122,17 @@ update_status ModuleSceneIntro::Update(float dt)
 	RenderBox(-10, { 1, 0, 0 }, { 0, 1, 100 }, { 40,1,20 }, Gray);
 	RenderBox(-30, { 1, 0, 0 }, { 0, 1, 950 }, { 40,1,20 }, Gray);
 	//trees
-	for (float i = 0; i < 8; i++)
+	for (float i = 0; i < 7; i++)
 	{
 		float distance = i * 100;
 		RenderBox(0, { 0, 1, 0 }, { 10, 3, distance }, { 1,10,1 }, Brown);
 		RenderBox(0, { 0, 1, 0 }, { 10, 9, distance }, { 6,6,6 }, Deep_Green);
 
-		RenderBox(0, { 0, 1, 0 }, { -5, 3, distance + 20 }, { 1,10,1 }, Brown);
-		RenderBox(0, { 0, 1, 0 }, { -5, 9, distance + 20 }, { 6,6,6 }, Deep_Green);
+		RenderBox(0, { 0, 1, 0 }, { -5, 3, distance + 40 }, { 1,10,1 }, Brown);
+		RenderBox(0, { 0, 1, 0 }, { -5, 9, distance + 40 }, { 6,6,6 }, Deep_Green);
 
-		RenderBox(0, { 0, 1, 0 }, { -5, 3, distance + 70 }, { 1,10,1 }, Brown);
-		RenderBox(0, { 0, 1, 0 }, { -5, 9, distance + 70 }, { 6,6,6 }, Deep_Green);
+		RenderBox(0, { 0, 1, 0 }, { -5, 3, distance + 90 }, { 1,10,1 }, Brown);
+		RenderBox(0, { 0, 1, 0 }, { -5, 9, distance + 90 }, { 6,6,6 }, Deep_Green);
 
 		RenderBox(0, { 0, 1, 0 }, { 5, 3, distance + 120 }, { 1,10,1 }, Brown);
 		RenderBox(0, { 0, 1, 0 }, { 5, 9, distance + 120 }, { 6,6,6 }, Deep_Green);
@@ -117,13 +140,36 @@ update_status ModuleSceneIntro::Update(float dt)
 		RenderBox(0, { 0, 1, 0 }, { -15, 3, distance + 110 }, { 1,10,1 }, Brown);
 		RenderBox(0, { 0, 1, 0 }, { -15, 9, distance + 110 }, { 6,6,6 }, Deep_Green);
 
-		RenderBox(0, { 0, 1, 0 }, { 15, 3, distance + 140 }, { 1,10,1 }, Brown);
-		RenderBox(0, { 0, 1, 0 }, { 15, 9, distance + 140 }, { 6,6,6 }, Deep_Green);
+		RenderBox(0, { 0, 1, 0 }, { 15, 3, distance + 160 }, { 1,10,1 }, Brown);
+		RenderBox(0, { 0, 1, 0 }, { 15, 9, distance + 160 }, { 6,6,6 }, Deep_Green);
 
-		RenderBox(0, { 0, 1, 0 }, { 0, 3, distance + 160 }, { 1,10,1 }, Brown);
-		RenderBox(0, { 0, 1, 0 }, { 0, 9, distance + 160 }, { 6,6,6 }, Deep_Green);
+		RenderBox(0, { 0, 1, 0 }, { 0, 3, distance + 205 }, { 1,10,1 }, Brown);
+		RenderBox(0, { 0, 1, 0 }, { 0, 9, distance + 205 }, { 6,6,6 }, Deep_Green);
 	}
-	
+	//people
+	RenderBox(20, { 0, 0, 1 }, { 10, 3, 50 }, { 1,4,1 }, Black);
+	RenderBox(-20, { 0, 0, 1 }, { 8, 3, 50 }, { 1,4,1 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { 9, 7, 50 }, { 1,5,1 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { 9, 9.5, 50 }, { 2,2,2 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { 9, 7.5, 50 }, { 4,1,1 }, Black);
+
+	RenderBox(20, { 0, 0, 1 }, { -10, 3, 200 }, { 1,4,1 }, Black);
+	RenderBox(-20, { 0, 0, 1 }, { -12, 3, 200 }, { 1,4,1 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { -11, 7, 200 }, { 1,5,1 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { -11, 9.5, 200 }, { 2,2,2 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { -11, 7.5, 200 }, { 4,1,1 }, Black);
+
+	RenderBox(20, { 0, 0, 1 }, { 5, 3, 450 }, { 1,4,1 }, Black);
+	RenderBox(-20, { 0, 0, 1 }, { 3, 3, 450 }, { 1,4,1 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { 4, 7, 450 }, { 1,5,1 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { 4, 9.5, 450 }, { 2,2,2 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { 4, 7.5, 450 }, { 4,1,1 }, Black);
+
+	RenderBox(20, { 0, 0, 1 }, { -7, 3, 700 }, { 1,4,1 }, Black);
+	RenderBox(-20, { 0, 0, 1 }, { -9, 3, 700 }, { 1,4,1 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { -8, 7, 700 }, { 1,5,1 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { -8, 9.5, 700 }, { 2,2,2 }, Black);
+	RenderBox(0, { 0, 0, 1 }, { -8, 7.5, 700 }, { 4,1,1 }, Black);
 
 
 
